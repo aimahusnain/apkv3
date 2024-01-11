cbc79b49036c7919cb422f57e91342edc4183764
aimahusnain <aimahusnain@gmail.com>
2024-01-11 12:01:26 +0500

    sfdb

--- src/app/blogs/category/[id]/page.tsx
+++ src/app/blogs/category/[id]/page.tsx
@@ -1,17 +1,6 @@
 import BlogList from "@/components/blogs/blog-list";
 import CategoryList from "@/components/category";
 
-async function extractAllBlogs() {
-  const res = await fetch(`${process.env.URL}/api/blog-post/get-all-post`, {
-    method: "GET",
-    cache: "no-store",
-  });
-
-  const data = await res.json();
-
-  if (data.success) return data.data;
-}
-
 async function getAllListsByCategory(getId: string) {
   const res = await fetch(`${process.env.URL}/api/category?categoryID=${getId}`, {
     method: "GET",
