9378373df89b15d2162699950366c873b67b94f6
aimahusnain <aimahusnain@gmail.com>
2024-01-12 12:39:03 +0500

    dsf

--- src/app/api/category/route.ts
+++ src/app/api/category/route.ts
@@ -1,34 +1,34 @@
-export const dynamic = 'force-dynamic'
 import prisma from "@/database";
 import { NextRequest, NextResponse } from "next/server";
 
 export const GET = async (request: NextRequest) => {
   try {
-    const { searchParams } = new URL(request.url)
-    const getCategoryId = searchParams.get('categoryId')
+    const { searchParams } = new URL(request.url);
+    const extractCategoryID = searchParams.get("categoryID");
 
-    const getPostsFromCategoryId = await prisma.post.findMany({
+    const getBlogPostListBasedOnCurrentCategoryID = await prisma.post.findMany({
       where: {
-        category: getCategoryId || ''
-      }
-    })
+        category: extractCategoryID || "",
+      },
+    });
 
-    if (getPostsFromCategoryId) {
+    if (getBlogPostListBasedOnCurrentCategoryID) {
       return NextResponse.json({
         success: true,
-        data: getPostsFromCategoryId
-      })
+        data: getBlogPostListBasedOnCurrentCategoryID,
+      });
     } else {
       return NextResponse.json({
         success: false,
-        message: 'Failed to fetch data!'
-      })
+        message: "Failed to fetch data ! Please try again",
+      });
     }
   } catch (error) {
-    console.log(error)
+    console.log(error);
+
     return NextResponse.json({
       success: false,
-      message: 'Something went wrong, please try again!'
-    })
+      message: "Something went wrong ! Please try again",
+    });
   }
 }
