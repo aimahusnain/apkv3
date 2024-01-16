273d23b76eb0405973632cd84a651c2d7d57c9fb
aimahusnain <aimahusnain@gmail.com>
2024-01-17 03:58:53 +0500

    sdfsdf

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -13,25 +13,31 @@ const Footer = () => {
   return (
     <div className="flex flex-col text-center p-8">
       <div className="h-fit flex gap-14 py-8 justify-center">
+        <Tooltip content="Instagram">
+            <Link target="_blank" href="https://www.instagram.com/alightmotion">
+              <FaInstagram size={20} className="hover:scale-125 transition-all" />
+            </Link>
+        </Tooltip>
+        
         <Tooltip content="Twitter">
             <Link target="_blank" href="https://twitter.com/alightcreative">
-              <RiTwitterXFill size={40} className="hover:scale-125 transition-all" />
+              <RiTwitterXFill size={20} className="hover:scale-125 transition-all" />
             </Link>
         </Tooltip>
 
         <Tooltip content="Facebook">
           <Link target="_blank" href="https://www.facebook.com/alightcreative">
-            <MdOutlineFacebook size={40} className="hover:scale-125 transition-all" />
+            <MdOutlineFacebook size={20} className="hover:scale-125 transition-all" />
           </Link>
         </Tooltip>
 
         <Tooltip content="Youtube">
           <Link target="_blank" href="https://youtube.com/alightmotion">
-            <SlSocialYoutube size={40} className="hover:scale-125 transition-all w-8" />
+            <SlSocialYoutube size={20} className="hover:scale-125 transition-all w-8" />
           </Link>
         </Tooltip>
       </div>
-      <p>
+      <p className="text-white/40 text-xs">
         Copyright © Splice Video Editor S.r.l. | Corso Como 15, 20154 Milan,
         Italy | VAT, tax code, and number of registration with the Milan Monza
         Brianza Lodi Company Register 11505810967 | REA number MI 2608304 |
